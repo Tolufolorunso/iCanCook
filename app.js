3b6e091604f3801898747461d8bbf0359d56e4c0
Tolulope Folorunso <tolufolorunso@yahoo.com>
2021-01-21 13:22:14 +0100

    Security and user routes implemented

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controllers/errorController.js');
 
@@ -7,12 +12,34 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
-// MIDDLEWARES
+// 1) GLOBAL MIDDLEWARE
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'developmente') {
 	app.use(morgan('dev'));
 }
-console.log(process.env.NODE_ENV);
-app.use(express.json());
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP, please try again in an hour'
+});
+app.use('/', limiter);
+
+app.use(express.json({ limit: '50kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+	hpp({
+		whitelist: ['rating']
+	})
+);
 
 app.use((req, res, next) => {
 	const date = new Date().getFullYear();
